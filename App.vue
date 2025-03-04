3ab2707d1756bc950c42b670069a0be625171998
chatc <chatchom864@gmail.com>
2025-03-04 07:12:18 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <NavbarComp />
+  <router-view />
+</template>
+
+<script>
+import NavbarComp from "./components/NavbarComp.vue";
+
+
+export default {
+  name: "App",
+
+  components: {
+    NavbarComp,
+    
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
